bbe799fe187ee4ee33ef99c776234274b2b13083
dagerebe <rebeccadagenais@Rebeccas-MacBook-Pro-3.local>
2015-02-24 14:54:11 -0500

    created new function and fixed space for grows on shrub

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var blueberries = {
 	name: 'Blueberries',
 	name2: 'Vaccinium Corymbosum',
-	Grows_On_Shrub: 'Yes',
+	grows_on_shrub: 'Yes',
 	energy: '240 kJ',
 	carbohydrates: '14.49 g',
 	protein: '0.74 g',
@@ -9,7 +9,7 @@ var blueberries = {
 var grapes = {
 	name: 'Grapes',
 	name2: 'Vitis Vinifera',
-	Grows_On_Shrub: 'No',
+	grows_on_shrub: 'No',
 	energy: '288 kJ',
 	carbohydrates: '18.1 g',
 	protein: '0.72 g'
@@ -18,23 +18,25 @@ var grapes = {
 var redcurrant = {
 	name: 'Red Currant',
 	name2: 'Ribes Rubrum',
-	Grows_On_Shrub: 'Yes',
+	grows_on_shrub: 'Yes',
 	energy: '234 kJ',
 	carbohydrates: '13.8 g',
 	protein: '1.4 g',
 };
 
-var fruit = [blueberries, grapes, redcurrant];
+var writeFruit = function (fruit) {
+	fruit.forEach(function (item) {
+		document.write('<h2>' + item.name + '</h2>');
+		document.write('<h4>' + item.name2 + '</h4>');
+		document.write('<dl>');
+		document.write('<dt>Grows On Shrub:</dt><dd>' + item.grows_on_shrub + '</dd>');
+		document.write('<dt>Energy:</dt><dd>' + item.energy + '</dd>');
+		document.write('<dt>Carbohydrates:</dt><dd>' + item.carbohydrates + '</dd>');
+		document.write('<dt>Protein:</dt><dd>' + item.protein + '</dd>');
+	});
+};
 
-fruit.forEach(function (item) {
-	document.write('<h2>' + item.name + '</h2>');
-	document.write('<h4>' + item.name2 + '</h4>');
-	document.write('<dl>');
-	document.write('<dt>Grows_On_Shrub:</dt><dd>' + item.grows_on_shrub + '</dd>');
-	document.write('<dt>energy:</dt><dd>' + item.energy + '</dd>');
-	document.write('<dt>carbohydrates:</dt><dd>' + item.carbohydrates + '</dd>');
-	document.write('<dt>protein:</dt><dd>' + item.protein + '</dd>');
-});
+writeFruit([blueberries, grapes, redcurrant]);
 
 
 
